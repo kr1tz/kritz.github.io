31689e3b2ebf55bf8f368ed8338daa3ece905d6e
Kris Trinidad <kristrinidad@gmail.com>
2019-01-29 00:06:50 -0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,29 @@
-$(document).ready(function() {
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: 'YOURUSERID',
-        limit: 12,
-        resolution: 'standard_resolution',
-        accessToken: 'YOUR ACCESS ID',
-        sortBy: 'most-recent',
-        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-    });
-
-
-    userFeed.run();
-
+$( document ).ready(function()
+{
     
-    // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
+    var userFeed = new Instafeed({
+    get: 'user',
+    userId: '5395660838',
+    limit:12,
+    resolution: 'standard_resolution',
+    accessToken: '5395660838.1677ed0.f283ce8b7c024cc3a58b1e5ed90799a9',
+    sortBy: 'most-recent',
+    template: '<div class="col-lg-4 gallery instaimg"><a here="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
+    
     });
+userFeed.run();
 
-
+$('gallery').magnificPopup({
+    type:'image',
+    gallery: {
+        enabled:true
+    }
+    });
 });
+
